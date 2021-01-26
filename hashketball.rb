f685b9d7eb09a1e43b157551bb9525cc9e4e0c45
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-26 22:43:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -47,15 +47,16 @@ def game_hash
       :players => [
         {
           :player_name => "Jeff Adrien",
-          :number => 0,
-          :shoe => 16,
-          :points => 22,
-          :rebounds => 12,
-          :assists => 12,
-          :steals => 3,
-          :blocks => 1,
-          :slam_dunks => 1
-        }]
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assists => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2
+        }
+      ]
     }
     
   }
