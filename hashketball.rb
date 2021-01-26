7a89101096a23fc351ef93535a037cc8be8e82a5
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-26 22:44:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -23,7 +23,7 @@ def game_hash
           :shoe => 14, 
           :points => 12,
           :rebounds => 12,
-          :assits => 12,
+          :assists => 12,
           :steals => 12,
           :blocks => 12,
           :slam_dunks => 7
