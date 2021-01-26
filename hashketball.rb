68101f5771af944bb6ea77c17e8627a848d77b4e
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-26 22:35:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -16,6 +16,9 @@ def game_hash
           :steals => 3,
           :blocks => 1,
           :slam_dunks => 1
+        },
+        {
+          :player_name => "Reggie Evans"
         }
       ]
     },
