d28727a9f13dc3b7ccb4cc07b4daf0f00064eeff
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-26 22:27:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -8,7 +8,7 @@ def game_hash
       :players => [
         {
           :player_name => "Alan Anderson",
-          :number => 
+          :number => 0,
           :shoe =>
           :points =>
           :rebounds =>
