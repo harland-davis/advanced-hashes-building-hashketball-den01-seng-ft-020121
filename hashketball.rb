59f444a2524f947f429c0ae0857d73bbb98a017a
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-26 22:58:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -102,14 +102,25 @@ def game_hash
         },
         {
           :player_name => "Ben Gordon",
-          :number => 11,
-          :shoe => 17, 
-          :points => 17,
-          :rebounds => 19,
-          :assists => 10,
-          :steals => 3,
+          :number => 8,
+          :shoe => 15, 
+          :points => 33,
+          :rebounds => 3,
+          :assists => 2,
+          :steals => 1,
           :blocks => 1,
-          :slam_dunks => 15
+          :slam_dunks => 0
+        },
+        {
+          :player_name => "Kemba Walker",
+          :number => 33,
+          :shoe => 15, 
+          :points => 6,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 7,
+          :blocks => 5,
+          :slam_dunks => 12
         }
       ]
     }
